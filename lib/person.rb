d2865101c0b4ffa432c535b25eb15a6b2a450e01
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-11 14:48:43 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(this_persons_name)
+    @name = this_persons_name
+  end 
+  
+  def name 
+    @name
+  end 
+
+  def job=(job_type)
+    @job = job_type
+  end 
+  
+  def job 
+    @job 
+  end 
+end 
